07ad0bd25d99a6b58fc9673ba0998b92ae05812b
Chester <chez8990@gmail.com>
2019-11-01 00:15:21 +0800

    working scraper

--- AnnualReportScraper/spiders/annual_report_spider.py
+++ AnnualReportScraper/spiders/annual_report_spider.py
@@ -1,25 +1,37 @@
-import scrapy
-import pandas as pd
-import time
+
+from __future__ import absolute_import
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+import scrapy
+import os
+import pandas as pd
+import time
+import requests
 
-class ReportSpider:
+
+class ReportSpider(scrapy.Spider):
     name = "report_spider"
     allowed_domains = ['hkexnews.hk']
     start_urls = ['https://www.hkexnews.hk/index.htm']
 
     def __init__(self):
-        self.companies = pd.read_csv('../../companies.csv')
-        self.driver = webdriver.Chrome()
+        parent_dir = os.path.abspath(os.path.join(__file__, "../.."))
+
+        self.companies = pd.read_csv(os.path.join(parent_dir, 'data', 'companies.csv'))
+        self.save_path = os.path.join(parent_dir, 'data')
+
+        options = webdriver.ChromeOptions()
+
+        options.add_argument("--no-sandbox");
+        options.add_argument("--disable-dev-shm-usage");
+        self.driver = webdriver.Chrome(os.path.join(parent_dir, 'driver', 'chromedriver'), options=options)
 
     def parse(self, response):
-        self.driver.get(response.url)
-
         for _, stock in self.companies.iterrows():
+            self.driver.get(self.start_urls[0])
+            time.sleep(3)
 
-            query = stock['Stock Code']
-            query = query.iloc[0]
+            query = str(stock['Stock Code']).zfill(5)
 
             elem = self.driver.find_element_by_xpath('//*[@id="searchStockCode"]')
             elem.clear()
@@ -28,7 +40,6 @@ class ReportSpider:
             time.sleep(2)
 
             self.driver.find_element_by_xpath('//*[@id="autocomplete-list-0"]/div[1]/div[1]/table/tbody/tr[1]').click()
-            self.driver.find_element_by_xpath('//*[@id="date-picker"]/div[1]/b[1]/ul/li[21]/button').click()
             elem.submit()
 
             time.sleep(3)
@@ -36,18 +47,36 @@ class ReportSpider:
 
             while load_more is not None:
                 load_more.click()
-                time.sleep(4)
-                load_more = self.driver.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[1]/ul/li/a')
+                time.sleep(2)
+                try:
+                    load_more = self.driver.find_element_by_xpath('//*[@id="recordCountPanel2"]/div[1]/div/div[1]/ul/li/a')
+                except:
+                    break
 
             source = self.driver.page_source
             bsobj = bs(source, 'html.parser')
             result_docs = bsobj.findAll('div', attrs={'class': 'doc-link'})
 
             reports_links = []
+            reports_names = []
 
             for row in result_docs:
                 if 'annual report' in row.text.lower():
                     reports_links.append(row.a.get('href'))
+                    reports_names.append(row.a.text)
 
-            return reports_links
+            for link, name in zip(reports_links, reports_names):
+                pdf_name = link.split('/')[-1]
+
+                # check if stock directory exists
+                if not os.path.isdir(os.path.join(self.save_path, query)):
+                    os.mkdir(os.path.join(self.save_path, query))
+
+                # download the reports pdf
+                try:
+                    response = requests.get('https://www1.'+self.allowed_domains[0]+link)
+                    with open(os.path.join(self.save_path, query, name+'.pdf'), 'wb') as f:
+                        f.write(response.content)
+                except:
+                    pass
 
